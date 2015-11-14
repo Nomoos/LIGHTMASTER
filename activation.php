2e5a5412f06c1303da49edcdb7b13646e4816d4a
hittl <hittl.r@gmail.com>
2015-11-14 21:01:01 +0100

    Fix activation.

--- activation.php
+++ activation.php
@@ -10,10 +10,10 @@ if(isset($_GET['login']) AND isset($_GET['code']))
 	$code = htmlspecialchars(trim($_GET['code']));
 	
 // Vybereme identifikátor uživatele abysme vytvořili aktivační kód
-	$query = mysql_query("SELECT `id` FROM `users` WHERE `login`='".$login."'");
-	if(mysql_num_rows($query)!=0)
+	$query = mysqli_query($dataconection, "SELECT `id` FROM `users` WHERE `login`='".$login."'");
+	if(mysqli_num_rows($query)!=0)
 	{
-		$result = mysql_fetch_assoc($query);
+		$result = mysqli_fetch_assoc($query);
 		$activation = md5($result['id']).md5($login);
 		
 // Porovnáme aktivační kód, který uživatel odesíla kliknutím na odkaz v e-mailu s nově vytvořeným
@@ -21,8 +21,8 @@ if(isset($_GET['login']) AND isset($_GET['code']))
 		{
 			
 // Pokud tyto kódy odpovídají, aktualizujeme tabulku users a nastavíme pole activation na 1. ( 1 - uživatel aktivován, 0 - uživatel není aktivován )
-			$query1 = mysql_query("UPDATE `users` SET `activation`=1 WHERE `login`='".$login."'");
-			if(!$query1) { echo mysql_error() . " - " . mysql_errno(); }
+			$query1 = mysqli_query($dataconection, "UPDATE `users` SET `activation`=1 WHERE `login`='".$login."'");
+			if(!$query1) { echo mysqli_error($dataconection) . " - " . mysqli_errno($dataconection); }
 			else {
 				echo "Váš e-mail byl úspěšně potvrzen, teď se můžete <a href=\"index.php\">přihlásit</a>.";
 			}
@@ -38,11 +38,11 @@ if(isset($_GET['login']) AND isset($_GET['code']))
 
 // Ještě odstraníme uživatele, kteři neaktivovali e-mail do 1 hodiny
 // Vybereme jejich obrázky, abysme je následně odstranili.
-$query2 = mysql_query("SELECT `avatar` FROM `users` WHERE `activation`=0 AND UNIX_TIMESTAMP() - UNIX_TIMESTAMP(date) > 3600");
-if(mysql_num_rows($query2) > 0)
+$query2 = mysqli_query($dataconection, "SELECT `avatar` FROM `users` WHERE `activation`=0 AND UNIX_TIMESTAMP() - UNIX_TIMESTAMP(date) > 3600");
+if(mysqli_num_rows($query2) > 0)
 {
 // projdeme cyklusem všechny záznamy
-	while($result2 = mysql_fetch_assoc($query2))
+	while($result2 = mysqli_fetch_assoc($query2))
 	{
 // pokud nemají vlastní obrázek, tak nic nebudeme dělat
 			if($result2['avatar'] == 'no_avatar.jpg')
@@ -54,6 +54,6 @@ if(mysql_num_rows($query2) > 0)
 			}
 	}
 // Odstraníme záznamy z databáze
-	mysql_query("DELETE FROM `users` WHERE `activation`=0 AND UNIX_TIMESTAMP() - UNIX_TIMESTAMP(date) > 3600");
+	mysqli_query($dataconection, "DELETE FROM `users` WHERE `activation`=0 AND UNIX_TIMESTAMP() - UNIX_TIMESTAMP(date) > 3600");
 } 
 ?>
